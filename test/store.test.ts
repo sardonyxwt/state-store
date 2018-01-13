cfa0ed29a203c2afcd7eb4799200ee22da0a86cc
Kirill Petkov <35194846+sardonyxwt@users.noreply.github.com>
2018-01-13 23:39:39 +0200

    format files

--- test/store.test.ts
+++ test/store.test.ts
@@ -3,48 +3,48 @@ import Store, {ROOT_SCOPE} from '../src';
 
 describe('Store', () => {
 
-    let scopeId: string;
-    let actionId: string;
-    let listenerId: string;
-    const testValue = 1000;
+  let scopeId: string;
+  let actionId: string;
+  let listenerId: string;
+  const testValue = 1000;
 
-    it('registerScope', () => {
-        scopeId = Store.registerScope('testScope');
-    });
+  it('registerScope', () => {
+    scopeId = Store.registerScope('testScope');
+  });
 
-    it('registerAction', () => {
-        actionId = Store.registerAction((scope, props, resolved) => {
-            resolved(props);
-        }, scopeId);
-    });
+  it('registerAction', () => {
+    actionId = Store.registerAction((scope, props, resolved) => {
+      resolved(props);
+    }, scopeId);
+  });
 
-    it('subscribe', () => {
-        listenerId = Store.subscribe(({ newScope }) => {
-            expect(newScope).toEqual(testValue);
-        }, scopeId);
-    });
+  it('subscribe', () => {
+    listenerId = Store.subscribe(({newScope}) => {
+      expect(newScope).toEqual(testValue);
+    }, scopeId);
+  });
 
-    it('dispatch', () => {
-        Store.dispatch(actionId, testValue).then(newScope => {
-            expect(newScope).toEqual(testValue);
-        });
+  it('dispatch', () => {
+    Store.dispatch(actionId, testValue).then(newScope => {
+      expect(newScope).toEqual(testValue);
     });
+  });
 
-    it('unsubscribe', () => {
-      Store.unsubscribe(listenerId);
-    });
+  it('unsubscribe', () => {
+    Store.unsubscribe(listenerId);
+  });
 
-    it('getScope', () => {
-        const scope = Store.getScope(scopeId);
-        expect(scope).toEqual(testValue);
-    });
+  it('getScope', () => {
+    const scope = Store.getScope(scopeId);
+    expect(scope).toEqual(testValue);
+  });
 
-    it('getState', () => {
-        const state = Store.getState();
-        expect(state).toEqual({
-            [ROOT_SCOPE]: {},
-            [scopeId]: testValue
-        });
+  it('getState', () => {
+    const state = Store.getState();
+    expect(state).toEqual({
+      [ROOT_SCOPE]: {},
+      [scopeId]: testValue
     });
+  });
 
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,36 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const plugins = [
-    new CleanWebpackPlugin('dist'),
-    new webpack.optimize.UglifyJsPlugin({
-        minimize: true,
-        sourceMap: true
-    }),
-    new BundleAnalyzerPlugin({
-        openAnalyzer: false,
-        analyzerMode: 'static',
-        reportFilename: `${__dirname}/bundle-report.html`
-    })
+  new CleanWebpackPlugin('dist'),
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    sourceMap: true
+  }),
+  new BundleAnalyzerPlugin({
+    openAnalyzer: false,
+    analyzerMode: 'static',
+    reportFilename: `${__dirname}/bundle-report.html`
+  })
 ];
 
 module.exports = {
-    entry: './src/index.ts',
-    devtool: 'source-map',
-    output: {
-        filename: "./dist/application-state-store.js",
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'awesome-typescript-loader'
-            }
-        ]
-    },
-    plugins
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  output: {
+    filename: "./dist/application-state-store.js",
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  plugins
 };
