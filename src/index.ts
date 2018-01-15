70ceaea992a40d5029445c37f32011f6e8bdfafa
Kirill Petkov <35194846+sardonyxwt@users.noreply.github.com>
2018-01-15 17:47:49 +0200

    change package.json version 1.1.4 => 1.1.5 add jsdoc change typings path dist/types/index.d.ts => src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,46 @@ import {uniqueId, deepFreeze, values} from './utils';
 
 type ListenerEventType = { newScope, oldScope, actionId: string };
 type ListenerType = (event: ListenerEventType) => void;
-type ActionType = (scope, props, resolved: (newScope) => void, rejected: (error) => void) => void;
+type ActionType = (scope, props, resolve: (newScope) => void, reject: (error) => void) => void;
 type Action = { scopeId: string, func: ActionType };
 type Listener = { scopeId: string, func: ListenerType };
 
-const scopes: { [id: string]: any } = {};
-const actions: { [id: string]: Action } = {};
-const listeners: { [id: string]: Listener } = {};
+const scopes: { [scopeId: string]: any } = {};
+const actions: { [actionId: string]: Action } = {};
+const listeners: { [listenerId: string]: Listener } = {};
 
+/**
+ * This scope id use by default.
+ * @type {string}
+ */
 export const ROOT_SCOPE = registerScope('rootScope');
 
-function registerScope(name = 'scope', initScope = {}) {
+/**
+ * Registers a new scope and return id.
+ * @param {string} name A prefix of create scope id.
+ * @param {any} initScopeState The initial scope state.
+ * By default use empty object.
+ * @return {string} Id from registered scope.
+ */
+function registerScope(name = 'scope', initScopeState = {}) {
   const scopeId = uniqueId(name);
-
-  if (scopeId in scopes) {
-    throw new Error(`This scope already exists ${scopeId}`);
-  }
-
-  scopes[scopeId] = deepFreeze(initScope);
+  scopes[scopeId] = deepFreeze(initScopeState);
   return scopeId;
 }
 
+/**
+ * Registers a new action in scope and return action id.
+ * @param {ActionType} action The action that changes the scope
+ * when it is called.
+ * @param {string} scopeId Id of the scope to register the action.
+ * By default use ROOT_SCOPE id.
+ * @return {string} Id from registered action.
+ */
 function registerAction(action: ActionType, scopeId = ROOT_SCOPE) {
-  const isScopeExists = scopeId in scopes;
+  const scope = scopes[scopeId];
 
-  if (!isScopeExists) {
-    throw new Error(`This scope not exists ${scopeId}`);
+  if (!scope) {
+    throw new Error(`Scope not exists ${scopeId}`);
   }
 
   const actionId = uniqueId('store_action');
@@ -35,6 +49,15 @@ function registerAction(action: ActionType, scopeId = ROOT_SCOPE) {
   return actionId;
 }
 
+/**
+ * Dispatches an action. It is the only way to trigger a scope change.
+ * @param {string} actionId Triggered action id.
+ * This action change scope and return new scope.
+ * You can use resolve to change the scope or reject to throw an exception.
+ * @param {any?} props Additional data for the correct operation of the action.
+ * @return {Promise<any>} You can use the promise to get a new state of scope
+ * or catch errors
+ */
 function dispatch(actionId: string, props?) {
   const action = actions[actionId];
 
@@ -57,6 +80,14 @@ function dispatch(actionId: string, props?) {
   });
 }
 
+/**
+ * Adds a scope change listener.
+ * It will be called any time an action is dispatched.
+ * @param {ListenerType} listener A callback to be invoked on every dispatch.
+ * @param {string} scopeId Id of the scope to subscribe the listener.
+ * By default use ROOT_SCOPE id.
+ * @return {string} A listener id to remove this change listener later.
+ */
 function subscribe(listener: ListenerType, scopeId = ROOT_SCOPE) {
   const scope = scopes[scopeId];
 
@@ -71,14 +102,28 @@ function subscribe(listener: ListenerType, scopeId = ROOT_SCOPE) {
   return listenerId;
 }
 
+/**
+ * Removes a scope change listener.
+ * @param {string} listenerId Id of the listener to delete.
+ */
 function unsubscribe(listenerId: string) {
   delete listeners[listenerId];
 }
 
+/**
+ * Returns scope state.
+ * @param {string} scopeId Id scope, to get the state.
+ * By default use ROOT_SCOPE id.
+ * @return {any} Scope state
+ */
 function getScope(scopeId = ROOT_SCOPE) {
   return scopes[scopeId];
 }
 
+/**
+ * Returns all scope states.
+ * @return {{string: any}} Scope states
+ */
 function getState() {
   return {...scopes};
 }
